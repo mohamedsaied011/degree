64b6f7f269ad7b83aad530f07b264f1db7bf01fb
Moham <Moham@LATITUDEE5440>
2022-02-04 22:55:31 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace degree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblen = new System.Windows.Forms.Label();
+            this.txten = new System.Windows.Forms.TextBox();
+            this.btnshowdgree = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblen
+            // 
+            this.lblen.Location = new System.Drawing.Point(536, 156);
+            this.lblen.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblen.Name = "lblen";
+            this.lblen.Size = new System.Drawing.Size(207, 41);
+            this.lblen.TabIndex = 0;
+            this.lblen.Text = "ادخل مجموع مادة الانجليزي";
+            this.lblen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblen.Click += new System.EventHandler(this.lblen_Click);
+            // 
+            // txten
+            // 
+            this.txten.Location = new System.Drawing.Point(400, 167);
+            this.txten.Name = "txten";
+            this.txten.Size = new System.Drawing.Size(127, 30);
+            this.txten.TabIndex = 1;
+            this.txten.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnshowdgree
+            // 
+            this.btnshowdgree.Location = new System.Drawing.Point(400, 269);
+            this.btnshowdgree.Name = "btnshowdgree";
+            this.btnshowdgree.Size = new System.Drawing.Size(127, 34);
+            this.btnshowdgree.TabIndex = 2;
+            this.btnshowdgree.Text = "اظهر النتيجة";
+            this.btnshowdgree.UseVisualStyleBackColor = true;
+            this.btnshowdgree.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1015, 534);
+            this.Controls.Add(this.btnshowdgree);
+            this.Controls.Add(this.txten);
+            this.Controls.Add(this.lblen);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblen;
+        private System.Windows.Forms.TextBox txten;
+        private System.Windows.Forms.Button btnshowdgree;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Convert;
+
+namespace degree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            decimal en = ToDecimal(txten.Text);
+
+            if (en >= 0 && en < 50)
+            {
+                MessageBox.Show(" انت راسب");
+            }
+            else if (en >= 50 && en < 60)
+            {
+                MessageBox.Show("تقديرك مقبول");
+            }
+            else if (en >= 60 && en < 70)
+            {
+                MessageBox.Show("تقديرك جيد ");
+            }
+            else if (en >= 70 && en < 85)
+            {
+                MessageBox.Show("تقديرك جيد جدا");
+            }
+            else if (en >= 85 && en == 100)
+            {
+                MessageBox.Show("تقديرك امتياز");
+            }
+            else
+            {
+                MessageBox.Show("انت كداب");
+            }
+
+        }
+    }
+}
